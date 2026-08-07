ccd0e03c937ccdea7cd3558956335ab9ec7a849b
Harsh <harshuttekarweb999@gmail.com>
2026-08-07 11:48:48 +0530

    chandge

--- curd_app/admin.py
+++ curd_app/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-# from curd_app.models import Employ_Att, Employ_Data 
+from curd_app.models import Employ_Att, Employ_Data 
 
 # Register your models here.
-# admin.site.register(Employ_Data)
-# admin.site.register(Employ_Att)
+admin.site.register(Employ_Data)
+admin.site.register(Employ_Att)

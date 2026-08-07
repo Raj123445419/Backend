1f90bc6e136ea8fa13fcfecb12256d1ee6a98b16
Harsh <harshuttekarweb999@gmail.com>
2026-08-07 11:10:44 +0530

    chandge

--- curd_opration/settings.py
+++ curd_opration/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
